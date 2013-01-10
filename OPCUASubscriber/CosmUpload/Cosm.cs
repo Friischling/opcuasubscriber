cfc7fa7173a8d9e9a2f5a87cb28be54b2817d331
larsoleruben <l.christoffersen@gmail.com>
2013-01-10 13:42:50 +0100

    First commit

---
+++ OPCUASubscriber/CosmUpload/Cosm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace OPCUASubscriber.CosmUpload
+{
+    class Cosm
+    {
+        const string baseUri = "http://api.cosm.com";
+        const string apiKey = "sN8NVF3oRQ4GnwxGd1YSLUPzn8-SAKxJa0hqdHhwNm1Vdz0g";
+        const string feedId = "/v2/feeds/68554";
+
+
+        public static void upload(string value)
+        {
+            value = "gtf,20";
+            try
+            {
+                WebRequest request = CreateRequest(value);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                string responseFromServer = reader.ReadToEnd();
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+                
+                Console.WriteLine(responseFromServer);
+            }
+            catch (Exception e)
+            {
+               Console.WriteLine( e.ToString());
+            }
+ 
+        }
+
+        static WebRequest CreateRequest( string value)
+        {
+            Console.WriteLine(value + " " + baseUri );
+            String jsonObj = @"{""version"":""1.0.0"", ""datastreams"":[{""current_value"":""27"".""id"":""15""}] }";
+            byte[] buffer = Encoding.ASCII.GetBytes(value);
+            
+            //HttpWebRequest request = (HttpWebRequest) HttpWebRequest.Create(baseUri+feedId);
+            WebRequest request = WebRequest.Create("http://api.cosm.com/v2/feeds/68554/gtf.csv");
+            
+            ((HttpWebRequest)request).KeepAlive = false;
+            ((HttpWebRequest) request).UserAgent = "Example Client";
+            request.Headers.Add("X-ApiKey", apiKey);
+            request.Method = "PUT";
+            request.ContentLength = buffer.Length;
+            request.ContentType = "text/csv";
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(buffer, 0, buffer.Length);
+            //dataStream.Flush();
+            dataStream.Close();
+            return request;
+        }
+    }
+}
---
+++ OPCUASubscriber/OpcUaTest.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Opc.Ua;
+using OpcLabs.EasyOpc.UA;
+using System.Threading;
+using OPCUASubscriber.CosmUpload;
+using System.IO;
+
+namespace OPCUASubscriber
+{
+    class OpcUaTest
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0 ) {
+                usage(); 
+            }
+            else
+            {
+                OpcUaTest opt = new OpcUaTest();
+                opt.SubscribeToUA(args);
+            }
+            
+        }
+
+        public static void usage() {
+            Console.WriteLine("First argument mut be path conf file, now stopping");
+            Thread.Sleep(2000);
+            Environment.Exit(0);
+        }
+
+
+        public void SubscribeToUA(String[] args)
+        {
+            //getting the nodeids and loading them
+            List<EasyUAMonitoredItemArguments> nodeList = readConf(args);
+            
+
+            
+            EasyUAClient easyUAClient = new EasyUAClient();
+            try
+            {
+                
+                
+                //NodeId ni = new NodeId("ns=2;s=PowerHub.LU1.WatchdogDevelopment");
+                UAEndpointDescriptor ud = new UAEndpointDescriptor("opc.tcp://test-opc-ua.powerhub.dk:32402");
+                EasyUAClient.EngineParameters.CertificateAcceptancePolicy.AcceptAnyCertificate = true;
+                easyUAClient.MonitoredItemChanged += easyUAClient_MonitoredItemChanged;
+                //EasyUAClient.EngineParameters.ApplicationCertificateStore = "";
+                //UANodeId nid = new UANodeId("ns=2;s=Danfoss.DANFOSS_01.WatchdogReceived");
+                //easyUAClient.SubscribeMonitoredItem(ud, nid, 1000);
+                //easyUAClient.MonitoredItemChanged += easyUAClient_MonitoredItemChanged;
+
+                easyUAClient.SubscribeMultipleMonitoredItems(nodeList.ToArray <EasyUAMonitoredItemArguments>());
+
+
+                //String Text = easyUAClient.ReadValue("opc.tcp://test-opc-ua.powerhub.dk:32402", nid).ToString();
+                //String Text = easyUAClient.ReadValue( ud, nid).ToString();
+            }
+            catch (Exception ee)
+            {
+                Console.WriteLine(ee.ToString());
+            }
+
+            while (true)
+            {
+                Thread.Sleep(100);
+            }
+
+
+        }
+
+        static void easyUAClient_MonitoredItemChanged(object sender, EasyUAMonitoredItemChangedEventArgs e)
+        {
+            // Display value
+            // Remark: Production code would check e.Exception before accessing e.AttributeData.
+            Console.WriteLine( e.Arguments.NodeId +" " + e.AttributeData.ServerTimestamp+" " + e.AttributeData.Value );
+        }
+
+        public List<EasyUAMonitoredItemArguments> readConf(String[] args)
+        {
+            var lines = File.ReadLines(args[0]).Select(a => a.Split(';'));
+            var csv = from line in lines
+                      select (from piece in line
+                              select piece);
+            //Build a list of the nodeId's
+            List<EasyUAMonitoredItemArguments> nodeList = new List<EasyUAMonitoredItemArguments>();
+            foreach (var item in csv) {
+                UANodeId nid = new UANodeId("ns=2;s=" + item.ToArray<String>()[0] + "." + item.ToArray<String>()[1] + "." + item.ToArray<String>()[2]);
+                UAEndpointDescriptor ud = new UAEndpointDescriptor("opc.tcp://test-opc-ua.powerhub.dk:32402");
+
+                EasyUAMonitoredItemArguments euia = new EasyUAMonitoredItemArguments(null, ud, nid);
+
+                nodeList.Add(euia);
+            }
+
+            return nodeList;
+        }
+
+
+    }
+
+
+}
